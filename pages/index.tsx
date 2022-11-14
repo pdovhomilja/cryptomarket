a191eeff65b51cecfde048a1b1714a2e50aaca3a
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-14 01:29:13 +0100

    bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,10 +24,10 @@ const Home: NextPage = () => {
       <Header />
       <main className="max-w-6xl mx-auto py-2 px-6">
         {loadingListings ? (
-          <div class="text-center">
+          <div className="text-center">
             <div role="status">
               <svg
-                class="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                className="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +41,7 @@ const Home: NextPage = () => {
                   fill="currentFill"
                 />
               </svg>
-              <span class="sr-only">Loading listings ...</span>
+              <span className="sr-only">Loading listings ...</span>
             </div>
           </div>
         ) : (
